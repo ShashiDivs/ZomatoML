067e59e60445846373c4020607d2c99b61137743
shashi <shashipolity@gmail.com>
2023-10-04 07:44:20 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,14 @@
 from src.constants import *
 from src.config.configuration import *
-import os,sys
+import os, sys
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
+
 
 
 @dataclass
@@ -36,16 +38,16 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.data_ingestion_config.test_data_path),exist_ok=True)
             test_set.to_csv(self.data_ingestion_config.test_data_path,header=True)
 
-            return(
-                self.data_ingestion_config.train_data_path,
-                self.data_ingestion_config.test_data_path
-            )
+            return(self.data_ingestion_config.train_data_path,self.data_ingestion_config.test_data_path)
 
 
         except Exception as e:
             raise CustomException(e,sys)
         
 
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initaite_data_transformation(train_data_path,test_data_path)
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -14,13 +14,26 @@ DATA_DIR_KEY = "finaltrain.csv"
 
 ARTIFACT_DIR_KEY = "Artifact"
 
-#Data ingestion related variable
+#Data ingestion related variables
 DATA_INGESTION_KEY = "data_ingestion"
 DATA_INGESTION_RAW_DATA_DIR = "raw_data_dir"
 DATA_IMGESTION_INGESTED_DATA_DIR_KEY = "ingested_dir"
 RAW_DATA_DIR_KEY = "raw.csv"
 TRAIN_DATA_DIR_KEY = "train.csv"
 TEST_DATA_DIR_KEY = "test.csv"
+
+
+#Data transformation related variables
+DATA_TRANSFORMATION_ARTIFACT = "data_transformation"
+DATA_PREPROCESSED_DIR = "processor"
+DATA_TRANSFORMATION_PROCESSING_OBJ = "processor.pkl"
+DATA_TRANSFORMATION_DIR = "trnasformation"
+TRANSFORMATION_TRAIN_DIR_KEY = "train.csv"
+TRANSFORMATION_TEST_DIR_KEY = "test.csv"
+
+
+#artifact / data_transformation /processor->processor.pkl-> and transforamtion
+#train.csv and test.csv
 
 
 
